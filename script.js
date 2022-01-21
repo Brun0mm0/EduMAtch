4f7405906c2e3f7464d96faba7a0037df00b5e66
Brun0mm0 <provenzanobruno@gmail.com>
2022-01-21 13:55:49 -0300

    responsive

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const nav = document.querySelector('.nav');
-const logo = document.querySelector('.nav__logo')
+const nav = document.querySelector('.navigation');
+const logo = document.querySelector('.navigation__logo')
 
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 
-const stickyNav = function (entries) {
+
+const stickyNav = (entries) => {
   const [entry] = entries;
   // console.log(entry);
 
-  if (!entry.isIntersecting) nav.classList.add('sticky', ),
+  if (!entry.isIntersecting) nav.classList.add('sticky'),
    logo.classList.add('sticky__logo');
 
   else nav.classList.remove('sticky'), 
@@ -20,7 +21,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-${navHeight}px`,
+  rootMargin: `-${navHeight+200}px`,
 });
 
 headerObserver.observe(header);
